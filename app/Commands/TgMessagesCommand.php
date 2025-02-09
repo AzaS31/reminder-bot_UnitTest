111ffbd0a5f850439ce416c757d39286ffab609a
Azamat <azamatsalaly@gmail.com>
2025-02-09 21:00:26 +0500

    added telegram

---
+++ app/Commands/TgMessagesCommand.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Command;
+
+use App\Application;
+use App\Telegram\TelegramApiImpl;
+
+class TgMessagesCommand extends Command {
+    protected Application $app;
+
+    public function __construct(Application $app) {
+        $this->app = $app;
+    }
+
+    function run(array $options = []): void {
+        $tgApi = new TelegramApiImpl($this->app->env('TELEGRAM_TOKEN'));
+        echo json_encode($tgApi->getMessages(0)); // Получаем сообщения с offset 0
+    }
+}
